e2bd1a00c4139c57c26584a9250d83a08988c365
Biswajit Ghosh <bg5050525@gmail.com>
2024-06-08 12:40:39 +0530

    adding more

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 let app = express()
 const PORT = 2000;
+app.get("/", async (req, res) => {
+  res.send("server is running ...")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
